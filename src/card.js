a6664e89f704aabf4b8936ee843041476af5e7db
Kong Yang <kmongy05@gmail.com>
2020-08-07 17:58:34 -0500

    Updated with logo and styling. Added js logic for potential clicks.

--- src/card.js
+++
@@ -1,5 +0,0 @@
-import { stats } from './stats.js';
-
-export let test = console.log('card it');
-
-console.log(stats);
--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import '/scss/main.scss';
-import * as card from './card.js';
+import { stats } from './stats.js';
+
+const card = document.querySelectorAll('.card');
+
+function cardFlip() {
+  const cardClick = event.currentTarget;
+
+  cardClick.classList.toggle('card-flip');
+}
+
+card.forEach((element) => element.addEventListener('click', cardFlip));
