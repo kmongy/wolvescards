b4e0b5405a6654992ceb646980bd568c5ba0ffa7
Kong Yang <kmongy05@gmail.com>
2020-08-11 14:26:47 -0500

    Updated with more player profiles and stats. Will need to finish it up.

--- src/main.js
+++ src/main.js
@@ -11,35 +11,46 @@ function addPlayerCardDOM() {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
     cardDiv.innerHTML = `
-    <div class="front front-player-img">
-          <div class="front-inner">
-            <div class="front-player-info">
-              <h3 class="player-name">${player.playerName}</h3>
-              <p>Timberwolves</p>
-            </div>
-            <div class="logo">
-              <img src="${logoURL}" alt="Timberwolves Logo">
-            </div>
-          </div>
+    <div class="front">
+      <div class="front-img-container">
+        <img class="front-player-img" src="${player.thumbnail}" alt="${player.playerName}">
+      </div>
+      <div class="front-inner">
+        <div class="front-player-info">
+          <h3 class="player-name">${player.playerName}</h3>
+          <p class="player-position">${player.position} - ${player.number}</p>
+          <p>Timberwolves</p>
         </div>
-        <div class="back">
-          <div class="back-inner">
-            <div class="back-player-stats">
-              <p>2019-2020 Season</p>
-              <ul>
-                <li class="pts">Pts: ${player.avgPts}</li>
-                <li class="pts">Rbds: ${player.avgRbds}</li>
-                <li class="pts">Ast: ${player.avgAst}</li>
-                <li class="pts">FG%: ${player.fgPercentage}</li>
-                <li class="pts">3P%: ${player.threeP}</li>
-                <li class="pts">FT%: ${player.ft}</li>
-                <li class="pts">Stl: ${player.stl}</li>
-                <li class="pts">Blk: ${player.blk}</li>
-                <li class="pts">GP: ${player.gp}</li>
-              </ul>
-            </div>
-          </div>
+        <div class="logo">
+          <img src="${logoURL}" alt="Timberwolves Logo">
         </div>
+      </div>
+    </div>
+    <div class="back">
+      <div class="back-inner">
+        <div class="back-player-stats">
+          <p>2019-2020 Season</p>
+          <ul>
+            <p>Stats</p>
+            <div class="stats-row">
+              <li class="pts">Pts: ${player.avgPts}</li>
+              <li class="pts">Rbds: ${player.avgRbds}</li>
+              <li class="pts">Ast: ${player.avgAst}</li>
+            </div>
+            <div class="stats-row">
+              <li class="pts">FG%: ${player.fgPercentage}</li>
+              <li class="pts">3P%: ${player.threeP}</li>
+              <li class="pts">FT%: ${player.ft}</li>
+            </div>
+            <div class="stats-row">
+              <li class="pts">Stl: ${player.stl}</li>
+              <li class="pts">Blk: ${player.blk}</li>
+              <li class="pts">GP: ${player.gp}</li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
     `;
 
     cardsContainer.appendChild(cardDiv);
